1bb958afae13b84789e334be8229afdce8788e2d
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-01 04:21:01 -0300

    Add a table view with countries

--- searchBariOS/searchBariOS/ViewController.swift
+++ searchBariOS/searchBariOS/ViewController.swift
@@ -10,30 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//    let countries = Country.getAllCountries()
+    let countries = Country.getAllCountries()
 
-//    lazy var tableView: UITableView = {
-//        let tableView = UITableView()
-//        tableView.translatesAutoresizingMaskIntoConstraints = false
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        tableView.register(CountryCell.self, forCellReuseIdentifier: "cell")
-//        return tableView
-//    }()
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CountryCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        setupUI()
+        setupAutolayout()
     }
 }
 
-//extension ViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
-//}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CountryCell else { return UITableViewCell() }
+        cell.titleLabel.text = countries[indexPath.row].title
+        cell.categoryLabel.text = countries[indexPath.row].continent
+
+        return cell
+    }
+}
+
+extension ViewController {
+    private func setupUI() {
+        view.addSubview(tableView)
+    }
+
+    private func setupAutolayout(){
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+}
 
